571c24c40eb0e8295b5acdd93c4565141b26d061
davidReward <preissdavid.de@gmail.com>
2022-10-10 08:25:10 +0200

    Prototyp 2

--- backup/clear_nas_access_u1.pas
+++ backup/clear_nas_access_u1.pas
@@ -5,7 +5,8 @@ unit clear_nas_access_u1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Windows,
+  ShellApi;
 
 type
 
@@ -13,8 +14,11 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
+    SEInfo: TShellExecuteInfo;
     function PlinkStarten : Integer;
   public
 
@@ -29,26 +33,28 @@ implementation
 
 { TForm1 }
 
-uses
-  Windows,ShellApi;
-
 procedure TForm1.Button1Click(Sender: TObject);
 begin
   PlinkStarten;
 end;
 
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  TerminateProcess(SEInfo.hProcess, 1);
+end;
+
 function TForm1.PlinkStarten: Integer;
 var
- SEInfo: LPShellExecuteInfo;
- ExitCode: DWORD;
- ExecuteFile, ParamString, StartInString: string;
+  ExitCode: DWORD;
+  ExecuteFile, ParamString, StartInString: string;
 begin
- //ShellExecute(0, nil, 'putty.exe', nil, '.', SW_SHOWNORMAL);
-  ExecuteFile:='putty.exe';
+  //ShellExecute(Handle, nil, 'putty.exe', nil, '.', SW_SHOWNORMAL);
+  //ExecuteFile:='putty.exe';
+  ExecuteFile:='plink.exe';
 
  FillChar(SEInfo, SizeOf(SEInfo), 0) ;
- //SEInfo.cbSize := SizeOf(LPShellExecuteInfo) ;
- with ^SEInfo do begin
+ SEInfo.cbSize := SizeOf(TShellExecuteInfo) ;
+ with SEInfo do begin
  fMask := SEE_MASK_NOCLOSEPROCESS;
  Wnd := Application.Handle;
  lpFile := PChar(ExecuteFile) ;
@@ -56,16 +62,17 @@ begin
 ParamString can contain the
 application parameters.
 }
-// lpParameters := PChar(ParamString) ;
+ lpParameters := PChar('-L 9999:192.168.42.2:80 -N pi@192.168.2.43 -pw kaas1234') ;
 {
 StartInString specifies the
 name of the working directory.
 If ommited, the current directory is used.
 }
-// lpDirectory := PChar(StartInString) ;
+ lpDirectory := PChar('.') ;
  nShow := SW_SHOWNORMAL;
  end;
- if ShellExecuteEx(SEInfo) then begin
+ ShellExecuteExA(@SEInfo)
+ {if ShellExecuteExA(@SEInfo) then begin
  repeat
  Application.ProcessMessages;
  GetExitCodeProcess(SEInfo.hProcess, ExitCode) ;
@@ -73,7 +80,7 @@ If ommited, the current directory is used.
  Application.Terminated;
  ShowMessage('Calculator terminated') ;
  end
- else ShowMessage('Error starting Calc!') ;
+ else ShowMessage('Error starting Calc!') ;}
 end;
 
 end.
--- clear_nas_access_u1.pas
+++ clear_nas_access_u1.pas
@@ -5,7 +5,8 @@ unit clear_nas_access_u1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Windows,
+  ShellApi;
 
 type
 
@@ -13,8 +14,11 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
+    SEInfo: TShellExecuteInfo;
     function PlinkStarten : Integer;
   public
 
@@ -29,21 +33,54 @@ implementation
 
 { TForm1 }
 
-uses
-  Windows,ShellApi;
-
 procedure TForm1.Button1Click(Sender: TObject);
 begin
   PlinkStarten;
 end;
 
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  TerminateProcess(SEInfo.hProcess, 1);
+end;
+
 function TForm1.PlinkStarten: Integer;
 var
- SEInfo: LPShellExecuteInfo;
- ExitCode: DWORD;
- ExecuteFile, ParamString, StartInString: string;
+  ExitCode: DWORD;
+  ExecuteFile, ParamString, StartInString: string;
 begin
- ShellExecute(0, nil, 'putty.exe', nil, '.', SW_SHOWNORMAL);
+  //ShellExecute(Handle, nil, 'putty.exe', nil, '.', SW_SHOWNORMAL);
+  //ExecuteFile:='putty.exe';
+  ExecuteFile:='plink.exe';
+
+ FillChar(SEInfo, SizeOf(SEInfo), 0) ;
+ SEInfo.cbSize := SizeOf(TShellExecuteInfo) ;
+ with SEInfo do begin
+ fMask := SEE_MASK_NOCLOSEPROCESS;
+ Wnd := Application.Handle;
+ lpFile := PChar(ExecuteFile) ;
+{
+ParamString can contain the
+application parameters.
+}
+ lpParameters := PChar('-L 9999:192.168.42.2:80 -N pi@192.168.2.43 -pw kaas1234') ;
+{
+StartInString specifies the
+name of the working directory.
+If ommited, the current directory is used.
+}
+ lpDirectory := PChar('.') ;
+ nShow := SW_HIDE;
+ end;
+ ShellExecuteExA(@SEInfo)
+ {if ShellExecuteExA(@SEInfo) then begin
+ repeat
+ Application.ProcessMessages;
+ GetExitCodeProcess(SEInfo.hProcess, ExitCode) ;
+ until (ExitCode <> STILL_ACTIVE) or
+ Application.Terminated;
+ ShowMessage('Calculator terminated') ;
+ end
+ else ShowMessage('Error starting Calc!') ;}
 end;
 
 end.
